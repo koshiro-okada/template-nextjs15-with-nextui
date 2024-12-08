d81c2cabcbe75869272bb24cda07088efb2b7e7b
Koshiro Okada <koshiro.okada.30@gmail.com>
2024-12-08 20:29:22 +0900

    feat: use nextui button component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "../styles/global.css";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NextUIProvider>{children}</NextUIProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@nextui-org/react";
 export default function Page() {
-  return <h1 className="text-red-500">Hello, Next.js!</h1>;
+  return (
+    <div>
+      <h1 className="text-red-500">Hello, Next.js!</h1>
+      <Button color="primary">Button</Button>
+    </div>
+  );
 }
